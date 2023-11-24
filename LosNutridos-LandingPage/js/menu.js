7adf59f249dcb0df9c8eb18e857c18d1319e7ff7
Felix Anampa <felix.anampa@gestionysistemas.com>
2023-11-24 01:33:39 -0500

    nuevas vistas

--- LosNutridos-LandingPage/js/menu.js
+++ LosNutridos-LandingPage/js/menu.js
@@ -1,15 +1,15 @@
-const enlaces = document.querySelectorAll("nav a");
-const secciones = document.querySelectorAll("section");
-enlaces.forEach(enlace => {
-    enlace.addEventListener("click", function(e) {
-        e.preventDefault(); 
-        const href = this.getAttribute("href");
-        secciones.forEach(seccion => {
-            seccion.style.display = "none";
-        });
-        const seccionMostrar = document.querySelector(href);
-        if (seccionMostrar) {
-            seccionMostrar.style.display = "block";
-        }
-    });
-});
+// const enlaces = document.querySelectorAll("nav a");
+// const secciones = document.querySelectorAll("section");
+// enlaces.forEach(enlace => {
+//     enlace.addEventListener("click", function(e) {
+//         e.preventDefault(); 
+//         const href = this.getAttribute("href");
+//         secciones.forEach(seccion => {
+//             seccion.style.display = "none";
+//         });
+//         const seccionMostrar = document.querySelector(href);
+//         if (seccionMostrar) {
+//             seccionMostrar.style.display = "block";
+//         }
+//     });
+// });
